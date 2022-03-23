d36db40887671ecb5ee4119ceb715d0982f1dcd1
Dion4286 <101573639+Dion4286@users.noreply.github.com>
2022-03-23 18:28:56 +0530

    Taxi
    Nama : Dionisius Rizky Pramusinto
    NIM : 21.11.4286
    Kelas : IF-07
    Subject : Praktikum 3 Pemrogaman

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi1 = new Taxi();
+            Taxi taxi2 = new Taxi();
+            Taxi taxi3 = new Taxi();
+            Taxi taxi4 = new Taxi();
+
+            taxi1.DriverName = " Dion ";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 13;
+
+            taxi2.DriverName = " Andi ";
+            taxi2.OnDuty = true;
+            taxi2.NumPassenger = 15;
+
+            taxi3.DriverName = " Tomo ";
+            taxi3.OnDuty = false;
+            taxi3.NumPassenger = 17;
+
+            taxi4.DriverName = " Aldi ";
+            taxi4.OnDuty = false;
+            taxi4.NumPassenger = 11;
+
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassenger();
+            taxi1.DropOffPassanger();
+
+            taxi2.TaxiInfo();
+            taxi2.PickUpPassenger();
+            taxi2.DropOffPassanger();
+
+            taxi3.TaxiInfo();
+            taxi3.PickUpPassenger();
+            taxi3.DropOffPassanger();
+
+            taxi4.TaxiInfo();
+            taxi4.PickUpPassenger();
+            taxi4.DropOffPassanger();
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+        public string Status { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Status = "Yes";
+            }
+            else
+            {
+                Status = "No";
+            }
+            Console.WriteLine("On Duty : {0}", Status);
+            Console.WriteLine("Num Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("**************************************");
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+            Console.WriteLine("**************************************\n\n");
+        }
+    }
+}
